98e27989e3489669353e0dbcf8f61b34f98d2b60
Deni Setyawan <deni.setyawan@gmail.com>
2026-03-29 23:26:28 +0700

    add api

---
+++ src/app/api/dashboard/employees/route.ts
@@ -0,0 +1,16 @@
+import { prisma } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  const users = await prisma.user.findMany({
+    orderBy: {
+      name: "asc",
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  });
+
+  return NextResponse.json(users);
+};
