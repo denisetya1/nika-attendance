ea5620a045b139d78710d56adbb0d1b88b3e0455
Deni Setyawan <deni.setyawan@gmail.com>
2026-06-01 01:11:13 +0700

    add is active query

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,12 +1,21 @@
-"use server"
+"use server";
 
 import { prisma } from "@/lib/db";
-import { forgotPasswordSchema, loginSchema, registerShcema, resetPasswordSchema, tokenSchema } from "@/lib/zod/user";
+import {
+  forgotPasswordSchema,
+  loginSchema,
+  registerShcema,
+  resetPasswordSchema,
+  tokenSchema,
+} from "@/lib/zod/user";
 import { hashSync } from "bcrypt-ts";
 import { AuthError } from "next-auth";
 import { signIn } from "@/auth";
 import { redirect } from "next/navigation";
-import { isRedirectError, RedirectType } from "next/dist/client/components/redirect";
+import {
+  isRedirectError,
+  RedirectType,
+} from "next/dist/client/components/redirect";
 import { sendMail } from "@/lib/mail";
 import { v4 as uuidv4 } from "uuid";
 import moment from "moment";
@@ -15,41 +24,43 @@ export const getEmployees = async () => {
   const employees = await prisma.user.findMany({
     where: {
       role: "employee",
+      isActive: true,
     },
     orderBy: {
-      name: 'asc'
+      name: "asc",
     },
     select: {
       id: true,
       name: true,
-      email: true
-    }
-  })
+      email: true,
+    },
+  });
 
   return employees;
-}
+};
 
 export const addUser = async (prevState: unknown, formData: FormData) => {
-
-  const validateFields = registerShcema.safeParse(Object.fromEntries(formData.entries()));
+  const validateFields = registerShcema.safeParse(
+    Object.fromEntries(formData.entries()),
+  );
 
   if (!validateFields.success) {
     return {
       errors: validateFields.error.flatten().fieldErrors,
-    }
+    };
   }
 
   const { name, email, password } = validateFields.data;
 
   const checkEmail = await prisma.user.findUnique({
     where: {
-      email
-    }
-  })
+      email,
+    },
+  });
   if (checkEmail) {
     return {
-      message: "Email sudah terdaftar!"
-    }
+      message: "Email sudah terdaftar!",
+    };
   }
 
   try {
@@ -57,73 +68,79 @@ export const addUser = async (prevState: unknown, formData: FormData) => {
       data: {
         name,
         email,
-        password: hashSync(password, 10)
-      }
-    })
+        password: hashSync(password, 10),
+      },
+    });
   } catch (error) {
     console.error(error);
 
     return {
-      message: `Pendaftaran gagal! <br /> ${error}.`
-    }
+      message: `Pendaftaran gagal! <br /> ${error}.`,
+    };
   }
 
   redirect("/login");
-}
+};
 
 export const loginUser = async (prevState: unknown, formData: FormData) => {
-
-  const validateFields = loginSchema.safeParse(Object.fromEntries(formData.entries()));
+  const validateFields = loginSchema.safeParse(
+    Object.fromEntries(formData.entries()),
+  );
 
   if (!validateFields.success) {
     return {
       errors: validateFields.error.flatten().fieldErrors,
-    }
+    };
   }
 
   const { email, password } = validateFields.data;
 
   try {
-    await signIn("credentials", { email, password, redirectTo: '/' });
+    await signIn("credentials", { email, password, redirectTo: "/" });
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
         case "CredentialsSignin":
           return {
-            message: "Email atau password tidak ditemukan!"
-          }
+            message: "Email atau password tidak ditemukan!",
+          };
         default:
           return {
-            message: "Tejadi kesalahan!"
-          }
+            message: "Tejadi kesalahan!",
+          };
       }
     }
 
     if (isRedirectError(error)) {
-      throw error
+      throw error;
     }
 
-    console.error('signin erorr', error);
+    console.error("signin erorr", error);
   }
 
   redirect("/", RedirectType.replace);
-}
+};
 
-export const forgotPassword = async (prevState: unknown, formData: FormData) => {
-  const validateFields = forgotPasswordSchema.safeParse(Object.fromEntries(formData.entries()));
+export const forgotPassword = async (
+  prevState: unknown,
+  formData: FormData,
+) => {
+  const validateFields = forgotPasswordSchema.safeParse(
+    Object.fromEntries(formData.entries()),
+  );
 
   if (!validateFields.success) {
     return {
       errors: validateFields.error.flatten().fieldErrors,
-    }
+    };
   }
 
   const { email } = validateFields.data;
 
   try {
     const user = await prisma.user.findUnique({
-      where: { email: email }
-    })
+      where: { email: email },
+    });
 
     if (user) {
       const token = Math.floor(Math.random() * 899999 + 100000);
@@ -134,51 +151,52 @@ export const forgotPassword = async (prevState: unknown, formData: FormData) => 
           userId: user.id,
           trxId,
           token: String(token),
-          expires: moment().add(10, 'minutes').toISOString()
-        }
-      })
+          expires: moment().add(10, "minutes").toISOString(),
+        },
+      });
 
       if (saveToken) {
         await sendMail({
-          from: 'no-reply@resend.beautycat.id',
+          from: "no-reply@resend.beautycat.id",
           to: user.email,
-          subject: 'Permintaan Reset Password',
+          subject: "Permintaan Reset Password",
           html: `<p>Hi ${user.name}</p><br /><p>Kami menerima permintaan reset password akun anda. <br />
           Berikut kode anda:</p>
           <p>&nbsp;</p>
           <p><span style="display:inline-block; padding: 8px 24px; background: #eee; font-size: 30px; font-weight:bold; letter-spacing: 10px">${token}</span></p>
           <p>&nbsp;</p>
-          <p>Abaikan email ini jika anda tidak melakukan permintan rest password.</p>`
-        })
+          <p>Abaikan email ini jika anda tidak melakukan permintan rest password.</p>`,
+        });
 
         return {
           message: JSON.stringify({
             success: true,
-            trxId
-          })
-        }
+            trxId,
+          }),
+        };
       }
     } else {
       return {
-        message: "Terjadi kesalahan!"
-      }
+        message: "Terjadi kesalahan!",
+      };
     }
-
   } catch (error) {
-    console.error("FORGOT_PASSWORD", error)
+    console.error("FORGOT_PASSWORD", error);
     return {
-      message: "Terjadi kesalahan!"
-    }
+      message: "Terjadi kesalahan!",
+    };
   }
-}
+};
 
 export const verifyToken = async (prevState: unknown, formData: FormData) => {
-  const validateFields = tokenSchema.safeParse(Object.fromEntries(formData.entries()));
+  const validateFields = tokenSchema.safeParse(
+    Object.fromEntries(formData.entries()),
+  );
 
   if (!validateFields.success) {
     return {
       errors: validateFields.error.flatten().fieldErrors,
-    }
+    };
   }
 
   const { token, trxId } = validateFields.data;
@@ -190,9 +208,9 @@ export const verifyToken = async (prevState: unknown, formData: FormData) => {
         trxId,
         expires: {
           gte: moment().toISOString(),
-        }
-      }
-    })
+        },
+      },
+    });
 
     if (verified) {
       return {
@@ -200,29 +218,30 @@ export const verifyToken = async (prevState: unknown, formData: FormData) => {
           success: true,
           token,
         }),
-      }
+      };
     } else {
       return {
         message: "Kode tidak valid!",
-      }
+      };
     }
-
   } catch (error) {
     console.error(error);
 
     return {
-      message: "Terjadi kesalahan!"
-    }
+      message: "Terjadi kesalahan!",
+    };
   }
-}
+};
 
 export const resetPassword = async (prevState: unknown, formData: FormData) => {
-  const validateFields = resetPasswordSchema.safeParse(Object.fromEntries(formData.entries()));
+  const validateFields = resetPasswordSchema.safeParse(
+    Object.fromEntries(formData.entries()),
+  );
 
   if (!validateFields.success) {
     return {
       errors: validateFields.error.flatten().fieldErrors,
-    }
+    };
   }
 
   const { token, password, trxId } = validateFields.data;
@@ -234,42 +253,41 @@ export const resetPassword = async (prevState: unknown, formData: FormData) => {
         trxId: trxId,
         expires: {
           gte: moment().toISOString(),
-        }
-      }
-    })
+        },
+      },
+    });
 
     if (verified) {
       const updatePass = await prisma.user.update({
         where: {
-          id: verified.userId
+          id: verified.userId,
         },
         data: {
-          password: hashSync(password, 10)
-        }
-      })
+          password: hashSync(password, 10),
+        },
+      });
 
       if (updatePass) {
         await prisma.forgotPassword.delete({
           where: {
-            id: verified.id
-          }
-        })
+            id: verified.id,
+          },
+        });
       }
 
       return {
         message: JSON.stringify({
           success: true,
-        })
-      }
+        }),
+      };
     }
-
   } catch (error) {
     console.error(error);
 
     return {
-      message: "Reset password gagal!"
-    }
+      message: "Reset password gagal!",
+    };
   }
 
   redirect("/login");
-}
+};
